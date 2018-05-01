4da9f2db4f0ae42927d7d6f1ba3b8c9134724215
Ewan Sinclair <somebloke789@gmail.com>
2018-05-01 01:41:46 -0400

    Cut down test boilerplate more

--- __tests__/api.js
+++ __tests__/api.js
@@ -6,12 +6,11 @@ const app = express()
 
 api.init(app, ()=>{
 
-	test('hello test', (done) => {
+	test('hello test', () => {
 		return supertest(app).get('/')
 		.expect(200)
 		.expect('Content-Type', /json/)
 		.expect('{"response":"Hello world!"}')
-		.end(done)
 	});
-	
+
 })
